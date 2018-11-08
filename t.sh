f985e999b1ef8eee8f116ad4a0eb450bc7fc7512
amalvishnunk <amalvishnunk@gmail.com>
2018-11-08 13:28:24 +0530

    day 2

---
+++ t.sh
@@ -0,0 +1,13 @@
+npm config get proxy
+npm config rm proxy
+npm config rm https-proxy
+npm config rm https_proxy
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset http_proxy
+unset https_proxy
+npm config get proxy
+sudo npm cache clear -f
+npm config set https-proxy http://192.168.1.25:8080
+npm config set proxy http://192.168.1.25:8080
+npm config set strict-ssl false
