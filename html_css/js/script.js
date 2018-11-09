38f1a3a56505a63de8239d769f4e4d7f4952d007
amalvishnunk <amalvishnunk@gmail.com>
2018-11-09 12:39:33 +0530

    day3

--- html_css/js/script.js
+++ html_css/js/script.js
@@ -3,7 +3,9 @@
     var rollno=document.getElementById("getno").value;
     var admno=document.getElementById("getadmno").value;
     var age=document.getElementById("getage").value;
-
+    var p=document.getElementById("getdistrict");
+    var dist=p.options[p.selectedIndex].value;
+console.log(dist);
     if(age<=18){//console.log("Not eligible");
     alert("Not eligible")}
     else{//console.log("Eligible");
